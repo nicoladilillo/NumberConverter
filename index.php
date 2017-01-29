43cd9329c7f3f0a48e3527bc282cbfddff03bbf9
Nicola Dilillo <nicoladilillo98@gmail.com>
2017-01-29 16:54:34 +0100

    Start create design

--- index.php
+++ index.php
@@ -1,9 +1,30 @@
 <?php $title = 'Binari&Decimali'; require('./partials/head.php'); ?>
 
-	<form action="numeri.php" method="POST" >
-		Decimali: <input type="number" name="decimale" ><br>
-		Binari(max 1111): <input type="number" name="binario" ><br>
-		<br><input type="submit" >
-	</form>
+	<h2>Conversione Numeri<small>quanti numeri da convertire'</small></h2>
+
+	<div class="container">
+
+		<form action="numeri.php" method="POST" >
+			
+			<div class="group">      
+		      <input type="number" name="decimale" required>
+		      <span class="highlight"></span>
+		      <span class="bar"></span>
+		      <label>Decimali</label>
+		    </div>
+
+			<div class="group">      
+		      <input type="text" name="binario" required>
+		      <span class="highlight"></span>
+		      <span class="bar"></span>
+		      <label>Binari</label>
+		    </div>
+
+			<input type="submit" >
+
+		</form>
+
+	</div>
 
 <?php require('./partials/tail.php'); ?>
+
--- partials/head.php
+++ partials/head.php
@@ -5,5 +5,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <title><?=$title;?></title>
+  <link rel="stylesheet" type="text/css" href="assets/css/master.css">
 </head>
 <body>

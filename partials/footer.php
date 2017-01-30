f8fba53921a3b04564d644150e070b44f98d397f
Dilillo Nicola <nicoladilillo98@gmail.com>
2017-01-30 09:21:16 +0100

    Add jquery CDN

--- partials/footer.php
+++ partials/footer.php
@@ -1,4 +1,8 @@
-  	<script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
+  	<script
+			  src="https://code.jquery.com/jquery-3.1.1.slim.min.js"
+			  integrity="sha256-/SIrNqv8h6QGKDuNoLGA4iret+kyesCkHGzVUUV0shc="
+			  crossorigin="anonymous"></script>
+	<script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
 	<script type="text/javascript" src="assets/js/myjs.js"></script>
 
 </body>
